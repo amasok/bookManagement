ade37d95183bc91e24c40f5ffa118887e55ab081
amasok <info@amasok.com>
2015-07-04 16:07:48 +0900

    setting generators

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,13 @@ module BookManagement
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.template_engine :slim
+      g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
+      g.fixture_replacement :factory_girl, dir: "spec/support/factories"
+    end
   end
 end
